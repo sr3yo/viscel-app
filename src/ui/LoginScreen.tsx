229e5a1dd42348771721d08aa8b3d5f15235b8c0
sr3yo <sawsib560@gmail.com>
2025-06-28 01:33:36 -0400

    updated login placement

--- src/ui/LoginScreen.tsx
+++ src/ui/LoginScreen.tsx
@@ -4,9 +4,19 @@ import "./LoginScreen.css";
 export default function LoginScreen() {
   return (
     <>
-      <div id="login-background">
-        <div id="login-container">
-          <h1>h</h1>
+      <div className="blob-outer-container">
+        <div className="blob-inner-container">
+          <div className="blob"></div>
+        </div>
+      </div>
+      <div id="entire-login-container">
+        <div id="welcome-message">
+          <h1>welcome to viscel</h1>
+        </div>
+        <div id="login-background">
+          <div id="login-container">
+            <h1>viscel</h1>
+          </div>
         </div>
       </div>
     </>
