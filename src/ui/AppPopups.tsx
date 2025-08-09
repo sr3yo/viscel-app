e364475d7ba27c476c96f4f9189c107ed23fec37
sr3yo <sawsib560@gmail.com>
2025-08-09 15:33:20 -0400

    modified list of desktop apps html

---
+++ src/ui/AppPopups.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState } from "react";
+
+export default function AppPopups({ onClose }: { onClose: () => void }) {
+  return (
+    <div className="popup-window">
+      <div className="popup-content">
+        <h2>App Popups</h2>
+
+        <button className="close-btn" onClick={onClose}>
+          x
+        </button>
+      </div>
+    </div>
+  );
+}
